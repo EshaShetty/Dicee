40f46ecade889a9abda2a54251a0e1d90f929a9b
Esha Shetty <you@example.com>
2020-10-21 20:06:49 -0400

    First Commit

--- app/src/main/java/com/example/dicee/MainActivity.java
+++ app/src/main/java/com/example/dicee/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.dicee;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //declaring variables
+        Button rollBtn;
+        final ImageView leftDice, rightDice;
+        final int die[] ={R.drawable.dice1,R.drawable.dice2,R.drawable.dice3,R.drawable.dice4,R.drawable.dice5,R.drawable.dice6};
+
+
+        //fetching the views by id
+        rollBtn = findViewById(R.id.rollBtn);
+        leftDice = findViewById(R.id.image_leftDice);
+        rightDice = findViewById(R.id.image_rightDice);
+
+        //set a button click
+        rollBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //System.out.println("Dice Rolled");
+                Random randomNumberGenerator = new Random();
+                int randomNum = randomNumberGenerator.nextInt(6);
+                leftDice.setImageResource(die[randomNum]);
+                randomNum = randomNumberGenerator.nextInt(6);
+                rightDice.setImageResource(die[randomNum]);
+            }
+        });
     }
 }
